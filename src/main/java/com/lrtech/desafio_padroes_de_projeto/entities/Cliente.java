c6fee2eaffd6bf8f88bcae20b552826681a76015
LucasRomero03 <romerocorrea@outlook.com>
2026-01-21 21:52:55 -0300

    FEAT: criacao do model Cliente

--- src/main/java/com/lrtech/desafio_padroes_de_projeto/entities/Cliente.java
+++ src/main/java/com/lrtech/desafio_padroes_de_projeto/entities/Cliente.java
@@ -1,5 +1,25 @@
 package com.lrtech.desafio_padroes_de_projeto.entities;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_Clintes")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Cliente {
-  
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+    private String nome;
+    private String email;
+    //TODO colocar como entidade endereço
+    private String endereço;
+
+
+
 }
