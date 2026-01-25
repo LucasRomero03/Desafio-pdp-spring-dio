b35648c85b552790ebe8aedec97612e5270bdd09
LucasRomero03 <romerocorrea@outlook.com>
2026-01-25 15:50:20 -0300

    Feat: serviço para enriquecer o cep

---
+++ src/main/java/com/lrtech/desafio_padroes_de_projeto/services/CepService.java
@@ -0,0 +1,14 @@
+package com.lrtech.desafio_padroes_de_projeto.services;
+
+import com.lrtech.desafio_padroes_de_projeto.entities.Endereco;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "viacep",url = "https://viacep.com.br/ws")
+public interface CepService {
+    //@RequestMapping(method = RequestMethod.GET, value = "/{cep}/json/")
+    @GetMapping(  "/{cep}/json/") //funciona tbm com as anotações do spring
+    public Endereco consultarEndereco(@PathVariable("cep") String cep);
+
+}
