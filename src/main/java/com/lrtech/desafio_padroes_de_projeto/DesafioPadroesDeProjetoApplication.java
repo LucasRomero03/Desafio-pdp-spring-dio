bc07a61de862c15765815b529e333bdfc0cc112b
LucasRomero03 <romerocorrea@outlook.com>
2026-01-25 15:49:52 -0300

    ajustes

--- src/main/java/com/lrtech/desafio_padroes_de_projeto/DesafioPadroesDeProjetoApplication.java
+++ src/main/java/com/lrtech/desafio_padroes_de_projeto/DesafioPadroesDeProjetoApplication.java
@@ -1,13 +1,20 @@
 package com.lrtech.desafio_padroes_de_projeto;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
-public class DesafioPadroesDeProjetoApplication {
+@EnableFeignClients
+public class DesafioPadroesDeProjetoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DesafioPadroesDeProjetoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("BACK END RODANDO");
+	}
 }
