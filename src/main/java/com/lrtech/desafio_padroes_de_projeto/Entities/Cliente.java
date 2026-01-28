67a10eca210e15f0c575cd12c9bd59772d537973
LucasRomero03 <romerocorrea@outlook.com>
2026-01-28 10:52:26 -0300

    feat: clinte implementando o userDetails do spring security

--- src/main/java/com/lrtech/desafio_padroes_de_projeto/Entities/Cliente.java
+++ src/main/java/com/lrtech/desafio_padroes_de_projeto/Entities/Cliente.java
@@ -1,8 +1,16 @@
 package com.lrtech.desafio_padroes_de_projeto.Entities;
 
+import com.lrtech.desafio_padroes_de_projeto.Entities.Enums.Role;
 import jakarta.persistence.*;
 import lombok.*;
+import org.apache.catalina.UserDatabase;
+import org.jspecify.annotations.Nullable;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
@@ -10,18 +18,57 @@ import java.util.UUID;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Cliente {
+public class Cliente  implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     private String nome;
     private String email;
+    private String senha;
+    private Role role;
 
     @ManyToOne
     @JoinColumn(name = "endereço_cep")
     private Endereco endereco;
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        if (role == Role.ADMIN) {
+            return List.of(new SimpleGrantedAuthority("ADMIN"), new SimpleGrantedAuthority("USER"));
+        }else{
+            return List.of(new SimpleGrantedAuthority("USER"));
+        }
+    }
 
+    @Override
+    public  String getPassword() {
+        return this.senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
